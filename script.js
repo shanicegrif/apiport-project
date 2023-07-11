6e30e6db4e97c9e63ef1233c3fa0d8bda706a772
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-07-10 22:09:49 -0400

    started js file

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById(filterForm).addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const filterId = document.getElementById('filteredId').value;
+    const filterName = document.getElementById('filterName').value;
+
+    
+})
