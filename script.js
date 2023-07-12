c4cd4a07a6dbf4aff809149b84dfeab8b4b511f9
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-07-11 20:14:02 -0400

    fixed formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://rickandmortyapi.com/api/character"
+const apiUrl = "https://rickandmortyapi.com/api/character";
 
 const characterId = document.querySelector(".characterId");
 const characterName = document.querySelector("input[name='characterName']");
@@ -7,59 +7,60 @@ const characterInfo = document.querySelector(".characterInfo");
 const button = document.querySelector("button");
 
 button.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("firing click");
+  e.preventDefault();
+  console.log("firing click");
 
-    const id = characterId.value;
-    const name = characterName.value;
-    const selectedStatus = document.querySelector("select").value;
+  const id = characterId.value;
+  const name = characterName.value;
+  const selectedStatus = document.querySelector("select").value;
 
-    console.log(selectedStatus);
+  console.log(selectedStatus);
 
-    const isForm1Filled = Array.from(document.querySelector(".form1").elements).some(function(element) {
-        return element.value !== "";
-    });
+  const isForm1Filled = Array.from(
+    document.querySelector(".form1").elements
+  ).some(function (element) {
+    return element.value !== "";
+  });
 
-    const isForm2Filled = Array.from(document.querySelector(".form2").elements).some(function(element) {
-        return element.value !== "";
-    });
+  const isForm2Filled = Array.from(
+    document.querySelector(".form2").elements
+  ).some(function (element) {
+    return element.value !== "";
+  });
 
-    if (isForm1Filled && isForm2Filled) {
-        alert("Only fill one form!");
-        
-    } else if (isForm1Filled) {
-        if (id) {
-            fetch(`${apiUrl}/${id}`)
-            .then(data => data.json())
-            .then(json => printCharacter(json))
-            .catch(err => alert("Something went wrong.", err));
-        }
-
-    } else if (isForm2Filled) {
-        let url = `${apiUrl}/?`;
-
-        if(name) {
-            url += `name=${name}&`;
-        }
-
-        if(selectedStatus) {
-            url += `status=${selectedStatus}&`
-        }
-
-        url = url.slice(0, -1);
-
-        fetch(url)
-        .then(data => data.json())
-        .then(json => filteredCharacters(json.results))
-        .catch(err => alert("Something went wrong.", err));
-
-    } else {
-        alert("Please fill out one of the forms!")
+  if (isForm1Filled && isForm2Filled) {
+    alert("Only fill one form!");
+  } else if (isForm1Filled) {
+    if (id) {
+      fetch(`${apiUrl}/${id}`)
+        .then((data) => data.json())
+        .then((json) => printCharacter(json))
+        .catch((err) => alert("Something went wrong.", err));
     }
+  } else if (isForm2Filled) {
+    let url = `${apiUrl}/?`;
+
+    if (name) {
+      url += `name=${name}&`;
+    }
+
+    if (selectedStatus) {
+      url += `status=${selectedStatus}&`;
+    }
+
+    url = url.slice(0, -1);
+
+    fetch(url)
+      .then((data) => data.json())
+      .then((json) => filteredCharacters(json.results))
+      .catch((err) => alert("Something went wrong.", err));
+  } else {
+    alert("Please fill out one of the forms!");
+  }
 });
 
 function printCharacter(character) {
-    characterInfo.innerHTML = `
+  characterInfo.innerHTML = `
     <article>
         <img src="${character.image}" alt="${character.name}"/>
         <h2>Name: ${character.name}</h2>
@@ -68,16 +69,16 @@ function printCharacter(character) {
         <p>Origin: ${character.origin.name}</p>
         <p>Status: ${character.status}</p>
     </article>
-    `  
-    document.querySelector(".form1").reset();
+    `;
+  document.querySelector(".form1").reset();
 }
 
 function filteredCharacters(characters) {
-    characterInfo.innerHTML = "";
+  characterInfo.innerHTML = "";
 
-    characters.forEach(character => {
-        const article = document.createElement("article");
-        article.innerHTML = `
+  characters.forEach((character) => {
+    const article = document.createElement("article");
+    article.innerHTML = `
             <img src="${character.image}" alt="${character.name}"/>
             <h2>Name: ${character.name}</h2>
             <p>Gender: ${character.gender}</p>
@@ -85,8 +86,8 @@ function filteredCharacters(characters) {
             <p>Origin: ${character.origin.name}</p>
             <p>Status: ${character.status}</p>
         `;
-        characterInfo.appendChild(article);
-    });
+    characterInfo.appendChild(article);
+  });
 
-    document.querySelector(".form2").reset();
+  document.querySelector(".form2").reset();
 }
